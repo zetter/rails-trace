0ae4d081176ef37e75af31a9129a5850ed5840aa
Chris Zetter <zetter@users.noreply.github.com>
2019-02-01 13:55:39 +0000

    Split out methods

---
+++ src/Method.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Method extends Component {
+  render() {
+    return (
+      <rect x={this.props.x} y={this.props.y * 8} width={this.props.width} height="6" stroke="none" fill="#AAA"/>
+    );
+  }
+}
+
+// {"x":2,"width":2,"y":2,"path":"<internal:prelude>","line_start":"72","line_end":"74","method":"read_nonblock","class":"Line"},{"x":5,"width":2,"y":2,"path":"/Users/zetter/.rbenv/versions/2.6.0/lib/ruby/gems/2.6.0/gems/puma-3.12.0/lib/puma/client.rb","line_start":"230","line_end":"257","method":"setup_body","class":"Line"}
+export default Method;
