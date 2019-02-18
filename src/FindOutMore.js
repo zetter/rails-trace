68229c53e669529d394567a888b09cbbe69beccb
Chris Zetter <zetter@users.noreply.github.com>
2019-02-18 12:13:52 +0000

    Load markdown

--- src/FindOutMore.js
+++ src/FindOutMore.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import {
-  Markdown
+  Markdown,
+  Box
 } from "grommet";
 
 import './Info.css'
 import lookup from './Metadata'
 
-
 class FindOutMore extends React.Component {
+  state = {};
+
+  componentDidMount() {
+    fetch(this.contentURL())
+      .then(response => response.text())
+      .then(markdown => this.setState({markdown: markdown}));
+  }
+
+  contentURL() {
+    const selected = this.props.selected;
+    const key = `${selected.class}-${selected.method}`.toLowerCase().replace(/[^a-z_-]/g, "-");
+    return `/find-out-more/${key}.md`
+  }
+
   render() {
-    return <Markdown>
-      ##hi
-    </Markdown>
+    if (this.state.markdown) {
+      return <Markdown>{this.state.markdown}</Markdown>
+    } else {
+      return <Box align="center">Loading</Box>
+    }
   }
 }
 
