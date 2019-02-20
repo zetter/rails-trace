fb6cdf3a460a7cb002e2da5cc9703e96e93d6ffe
Chris Zetter <zetter@users.noreply.github.com>
2019-02-20 17:13:18 +0000

    Remove out of date comment

--- src/Trace.js
+++ src/Trace.js
@@ -70,5 +70,4 @@ class Trace extends React.Component {
   }
 }
 
-// path":"<internal:prelude>","line_start":"72","line_end":"74","method":"read_nonblock","class":"Line"},{"x":5,"width":2,"y":2,"path":"/Users/zetter/.rbenv/versions/2.6.0/lib/ruby/gems/2.6.0/gems/puma-3.12.0/lib/puma/client.rb","line_start":"230","line_end":"257","method":"setup_body","class":"Line"
 export default Trace;
