445dfaaf9f0a5e833f8451bb159f625672d9231c
Chris Zetter <zetter@users.noreply.github.com>
2019-02-20 21:21:59 +0000

    Restyle and re-write info

--- src/Info.js
+++ src/Info.js
@@ -4,42 +4,46 @@ import {
   Tab,
   Tabs,
   Heading,
-  Paragraph
+  Paragraph,
+  Anchor
 } from "grommet";
 
-class Info extends React.Component {
+import './Info.css'
 
+class Info extends React.Component {
   render() {
     return <Box pad="medium" className="info">
-      <Tabs>
+      <Heading size="small" level="1" margin="0">Rails Trace</Heading>
+      <Tabs margin="0" justify="start" className="tabs">
         <Tab title="About">
-          <Box pad="medium">
-            <Heading margin="none">Rails Trace</Heading>
+          <Box pad="none">
             <Paragraph>
-              Have you ever wanted to know what happens when you request a page from a Ruby on Rails application?
+              What code runs when you request a page from a Ruby on Rails application?
             </Paragraph>
             <Paragraph>
-              Here you can follow every method call from the HTTP server, through Rack and into the Rails framework and to your application code.
-            </Paragraph>
-          </Box>
-        </Tab>
-        <Tab title="What does it show?">
-          <Box pad="medium">
-            <Paragraph>
-              This shows every method called on for a Ruby on Rails Rails Server running in the production environment for a simple application.
-            </Paragraph>
-            <Paragraph>
-              Flame & icicle graphs are often scaled to execution time to make it easier to spot performance problems. This graph is scaled so you can see every method invocation so it's easier to see how Rails works.
-            </Paragraph>
-            <Paragraph>
-              The execution of Ruby blocks, which pass the control flow to a previous point in the stack, are not shown. Much of the Ruby Standard Libary is written in C including the <code>String</code> and <code>Array</code> classes and calls to these and other C-functions are not shown either.
+              Here is an annotated trace showing every method call within Rails and it's dependencies.
+              It is for an example application that loads a blog post from the database and renders a page containing the post's title.
             </Paragraph>
           </Box>
         </Tab>
-        <Tab title="How was it made?">
-          <Box pad="medium">
+        <Tab title="What it shows">
+          <Box pad="none">
             <Paragraph>
-              The execution of Ruby blocks, which pass the control flow to a previous point in the stack, are not shown. Much of the Ruby Standard Libary is written in C including the <code>String</code> and <code>Array</code> classes and calls to these and other C-functions are not shown either.
+              This shows every one of the <strong>2041</strong> Ruby methods called by a Rails Server running in production mode while it responded to a HTTP request. These method calls were captured using the <Anchor href="https://ruby-doc.org/core-2.6/TracePoint.html">TracePoint class</Anchor>. Methods are colour-coded based on which Ruby Gem they are part of. 
+            </Paragraph>
+            <Paragraph>
+              <Anchor href="http://www.brendangregg.com/flamegraphs.html">Flame & icicle graphs</Anchor> like this are often scaled to execution time to make it possible to spot performance problems. This graph is scaled so you can see every method invocation so it's easier to follow how Rails works.
+            </Paragraph>
+          </Box>
+        </Tab>
+        <Tab title="What it doesn't show">
+          <Box pad="none">
+            <Paragraph>
+              The trace does not show how a Rails server starts. It also doesn't show some processes that Rails caches between requests such as processing the database schema or compiling view templates- showing all these too would make the trace even harder to navigate.
+            </Paragraph>
+            <Paragraph>
+              The execution of Ruby blocks, which pass the control flow to a previous point in the stack, are not shown.
+              Much of the Ruby Standard Libary is written in C including the <code>String</code> and <code>Array</code> classes. Calls to these and other C-functions are not shown.
             </Paragraph>
           </Box>
         </Tab>
